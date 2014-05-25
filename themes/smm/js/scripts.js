8e607a8458760ff1cecfa45020655ddcdd566c12
Jesse Kade <jessekade@gmail.com>
2014-05-25 12:54:40 -0700

    fixed smooth scrollTo issue

--- themes/smm/js/scripts.js
+++ themes/smm/js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-	$('.services-options, .jumbotron').onePageNav();
+	// $('.services-options, .jumbotron').onePageNav();
+
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 500);
+					return false;
+				}
+			}
+		});
+	});
 });
 
 $(window).load(function() {
